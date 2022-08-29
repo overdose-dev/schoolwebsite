4f13c2f4f0f465cd0cd4e4966f75ccd96052431b
overdose-dev <chinazauchenna@gmail.com>
2022-08-29 18:01:42 +0100

    updated

--- enrollScript.js
+++ enrollScript.js
@@ -1,1 +1,55 @@
-console.log("object");
+const firstName = document.querySelector("#firstName");
+const surname = document.querySelector("#surname");
+const gender = document.querySelector("#gender");
+const birthDate = document.querySelector("#birthDate");
+const contact = document.querySelector("#contact");
+const classes = document.querySelector("#classes");
+const ResAddress = document.querySelector("#ResAddress");
+
+function submitBtn() {
+  if (
+    !(
+      firstName.value === "" ||
+      surname.value === "" ||
+      gender.value === "" ||
+      birthDate.value === "" ||
+      contact.value === "" ||
+      classes.value === "" ||
+      ResAddress.value === ""
+    )
+  ) {
+    alert(
+      "Dear " +
+        firstName.value +
+        " " +
+        surname.value +
+        ", " +
+        "you have sucessfully enrolled"
+    );
+  } else {
+    alert("Your enrollment was not successful. Please try again later.");
+  }
+}
+
+// function runIt() {
+//   if (pass1.value == pass2.value) {
+//     alert("Login Successful");
+//   } else {
+//     alert("Login Not Successful");
+//   }
+// }
+
+// if (number.value == "") {
+//     text.textContent = "Input field cannot be empty";
+//     text.style.color = "#fff";
+//     text.style.display = "block";
+//   } else if (number.value.match(regex)) {
+//     text.textContent = "Congrats! You Entered A Valid Phone Number";
+//     text.style.display = "block";
+//     text.style.color = "white";
+//     location.href = "https://google.com";
+//   } else {
+//     text.textContent = "Ooops! Your Phone Number is Invalid";
+//     text.style.display = "block";
+//   }
+// });
